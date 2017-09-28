b2b35b53c25f2440c20fb45a772e1a0a3f73d58d
Solepride <leman.yan80@myhunter.cuny.edu>
2017-09-27 23:28:01 -0400

    3rd commit

--- 05/full_encode.py
+++ 05/full_encode.py
@@ -12,7 +12,7 @@ def full_encode(s):
     for i in range(len(s)):
         s.rotate(i)
         print(s)
-
+#but if i do return(s) it does not rotate for me. it does rotate 1 and 3 but not 2.
 print(full_encode('abc'))
 print(full_encode('abcd'))
 '''
@@ -27,4 +27,24 @@ print(full_encode('abc'))
 abc
 bca
 cab
+
+This code works for the video but not here for some reason
+
+import string
+import collections
+
+def caesar(rotate_string,number_to_rotate_by):
+    upper=collections.deque(string.ascii_uppercase)
+    lower=collections.deque(string.ascii_lowercase)
+    
+    upper.rotate(number_to_rotate_by)
+    lower.rotate(number_to_rotate_by)
+    
+    upper=''.join(list(upper))
+    lower=''.join(list(lower))
+    
+    return rotate_string.translate(string.maketrans(string.ascii_uppercase, upper)).translate(string.maketrans(string.ascii_lowercase, lower))
+
+
+print (caesar('This is too easy',1))
 '''
