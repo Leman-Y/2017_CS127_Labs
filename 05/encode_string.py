8c0a2ebae2a722da929760d3cbded3908d3b5e38
Solepride <leman.yan80@myhunter.cuny.edu>
2017-10-01 19:32:58 -0400

    second edit

--- 05/encode_string.py
+++ 05/encode_string.py
@@ -3,6 +3,9 @@ Write a routine encode_string(s,r) which return a new string which is the origin
 It should not rotate non-letters, those should just be copied over to the new string. Upper case letters should be rotated.
 You can convert them to lower case, rotate then convert back or you could make your rotate_letter routine handle it directly.
 '''
+'''
+THIS FILE DOES NOT WORK
+'''
 import collections
 
 def encode_string(string,r):
@@ -10,12 +13,33 @@ def encode_string(string,r):
     string=collections.deque(string)
     string.rotate(r)
     string=list(string)
-    return string
+    str1=''.join(string)
+    return str1
 
 print(encode_string('abc',1))
+print(encode_string('abc',2))
 print(encode_string('abcd',2))
 print(encode_string('abc1',1))
+
+#USE ENCODE LETTER IN ENCODE STRING
+#this does not work, because it does not give you the next letter, but it rotates the order
+def encode_letter(string,r):
+    result=''
+    for letter in string:
+        if ord(letter) in range(65,91) or ord(letter) in range(97,123):
+            result+=encode_string(letter,r)
+    else:
+        result+=letter
+    return result
+
+print(encode_letter('abc',1))
 '''
+result=''
+result+=''
+result=result+''
+
+
+
 My code does fulfill the conditions to rotate nonletters. I do not know how to isolate the nonletters then add the back to the end of the string.
 def encode_string(string,r):
     string=list(string)
@@ -23,4 +47,8 @@ def encode_string(string,r):
     string.rotate(r)
     if any(char.isdigit() for char in string)
     return string
+    
+NOTES 10/1/17:
+-This moved the order of the string. This did not rotate a letter n times and give back a new letter. I had to make a new program.
+
 '''
